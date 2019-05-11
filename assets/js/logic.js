a2289d7be1d970dbf3333c0ae473bc4812144ae2
willow <shaneweth77@gmail.com>
2019-05-10 21:33:44 -0400

    changed some values for shadow

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
-const walk = 100; // 100px
+const walk = 200; // 200px
 
 function shadow(e) {
     // const width = hero.offsetWidth;
@@ -28,9 +28,7 @@ function shadow(e) {
     const yWalk = Math.round((y / height * walk) - (walk / 2));
 
     text.style.textShadow = `
-        ${xWalk}px ${yWalk}px 0 red,
-        ${xWalk * -1}px ${yWalk}px 0 green,
-        ${xWalk}px ${yWalk * -1}px 0 blue
+        ${xWalk}px ${yWalk}px 0 rgba(100, 100, 100, .5)
         `
 
 }
