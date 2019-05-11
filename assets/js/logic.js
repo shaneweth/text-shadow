93577838a8a164072dbed027cea5c45305e1d04f
willow <shaneweth77@gmail.com>
2019-05-10 21:20:27 -0400

    added offsets to JS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,37 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
+const walk = 100; // 100px
 
 function shadow(e) {
     // const width = hero.offsetWidth;
     // const height = hero.offsetHeight;
 
-    const { offsetWidth: width, offsetHeight: height } = hero;
-    let { offsetX: x, offsetY: y } = e;
+    const {
+        offsetWidth: width,
+        offsetHeight: height
+    } = hero;
+    let {
+        offsetX: x,
+        offsetY: y
+    } = e;
 
-    
+
     console.log(this, e.target);
+    // if the target is not the same as this
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 red,
+        ${xWalk * -1}px ${yWalk}px 0 green,
+        ${xWalk}px ${yWalk * -1}px 0 blue
+        `
 
 }
 
